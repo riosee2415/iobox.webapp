cc08754dba99a9f3695d3fd75338f787d278df7c
4leaf.peb <4leaf.peb@gmail.com>
2021-11-02 20:35:39 +0900

    modify

--- front/components/AppFooter.jsx
+++ front/components/AppFooter.jsx
@@ -5,7 +5,6 @@ import {
   Text,
   GradientText,
   WholeWrapper,
-  RsWrapper,
 } from "./commonComponents";
 import Theme from "./Theme";
 import styled from "styled-components";
@@ -353,71 +352,60 @@ const AppFooter = () => {
                 visible={true}
                 getContainer={false}
               >
-                <Wrapper
-                  dr={`column`}
-                  height={`100vh`}
-                  al={`flex-start`}
-                  ju={`flex-start`}
-                >
-                  <RsWrapper
-                    height={`auto`}
-                    ju={`flex-start`}
-                    al={`flex-start`}
-                  >
-                    <Image
-                      src={`https://via.placeholder.com/100x100`}
-                      margin={`50px 0 100px 0`}
-                      width={`100px`}
-                      alt={`logo`}
-                    />
-                    <Wrapper al={`flex-start`} ju={`flex-start`}>
-                      <Wrapper al={`flex-start`}>
-                        <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
-                          아이오 박스란?
-                        </Text>
-                      </Wrapper>
-                      <Wrapper al={`flex-start`}>
-                        <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
-                          서비스 이용방법
-                        </Text>
-                      </Wrapper>
-                      <Wrapper al={`flex-start`}>
-                        <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
-                          서비스 이용료
-                        </Text>
-                      </Wrapper>
-                      <Wrapper al={`flex-start`}>
-                        <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
-                          IO박스 보관센터
-                        </Text>
-                      </Wrapper>
-                      <Wrapper al={`flex-start`}>
-                        <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
-                          배송현황
-                        </Text>
-                      </Wrapper>
-                      <Wrapper al={`flex-start`}>
-                        <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
-                          내 물건 찾기
-                        </Text>
-                      </Wrapper>
-                    </Wrapper>
-                  </RsWrapper>
+
+                <Image
+                  src={`https://via.placeholder.com/100x100`}
+                  margin={`50px 0 100px 40px`}
+                  width={`100px`}
+                  alt={`logo`}
+                />
+                <Wrapper padding={`0 0 0 30px`} al={`flex-start`}>
                   <Wrapper al={`flex-start`}>
-                    <Wrapper height={`30px`} bgColor={Theme.lightGrey_C}>
-                      1
-                    </Wrapper>
-                    <Wrapper height={`30px`} bgColor={Theme.lightGrey_C}>
-                      2
-                    </Wrapper>
-                    <Wrapper height={`30px`} bgColor={Theme.lightGrey_C}>
-                      3
-                    </Wrapper>
+                    <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
+                      아이오 박스란?
+                    </Text>
                   </Wrapper>
-                  <Wrapper dr={`row`}>
-                    <Text display={`flex`}>로그인</Text>
-                    <Text>고객센터</Text>
+                  <Wrapper al={`flex-start`}>
+                    <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
+                      서비스 이용방법
+                    </Text>
                   </Wrapper>
+                  <Wrapper al={`flex-start`}>
+                    <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
+                      서비스 이용료
+                    </Text>
+                  </Wrapper>
+                  <Wrapper al={`flex-start`}>
+                    <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
+                      IO박스 보관센터
+                    </Text>
+                  </Wrapper>
+                  <Wrapper al={`flex-start`}>
+                    <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
+                      배송현황
+                    </Text>
+                  </Wrapper>
+                  <Wrapper al={`flex-start`}>
+                    <Text fontSize={`1.4rem`} margin={`0 0 40px`}>
+                      내 물건 찾기
+                    </Text>
+                  </Wrapper>
+                </Wrapper>
+                <Wrapper al={`flex-start`}>
+                  <Wrapper height={`30px`} bgColor={Theme.lightGrey_C}>
+                    1
+                  </Wrapper>
+                  <Wrapper height={`30px`} bgColor={Theme.lightGrey_C}>
+                    2
+                  </Wrapper>
+                  <Wrapper height={`30px`} bgColor={Theme.lightGrey_C}>
+                    3
+                  </Wrapper>
+                </Wrapper>
+
+                <Wrapper dr={`row`}>
+                  <Text display={`flex`}>로그인</Text>
+                  <Text>고객센터</Text>
                 </Wrapper>
               </Drawer>
             )}
