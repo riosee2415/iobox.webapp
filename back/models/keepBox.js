9b3c4306d11fec63278a701db47fabf2dd425f89
신태섭 <sintaeseob@sintaeseob-ui-MacBookPro.local>
2021-11-22 14:24:07 +0900

    modify

--- back/models/keepBox.js
+++ back/models/keepBox.js
@@ -6,53 +6,53 @@ module.exports = class KeepBox extends Model {
     return super.init(
       {
         period: {
-          type: DataTypes.STRING(200),
+          type: DataTypes.STRING(200), // 보관 기간
           allowNull: false,
         },
         isPickup: {
-          type: DataTypes.BOOLEAN,
+          type: DataTypes.BOOLEAN, // 픽업 유무
           defaultValue: false,
           allowNull: false,
         },
         pickWay: {
-          type: DataTypes.STRING(300),
+          type: DataTypes.STRING(300), // 픽업 방식
           allowNull: false,
         },
         coupon: {
-          type: DataTypes.INTEGER,
+          type: DataTypes.INTEGER, // 쿠폰할인
           allowNull: false,
         },
         price: {
-          type: DataTypes.INTEGER,
+          type: DataTypes.INTEGER, // 금액
           allowNull: false,
         },
         name: {
-          type: DataTypes.STRING(50),
+          type: DataTypes.STRING(50), // 이름
           allowNull: false,
         },
         mobile: {
-          type: DataTypes.STRING(50),
+          type: DataTypes.STRING(50), // 전화번호
           allowNull: false,
         },
         address: {
-          type: DataTypes.STRING(300),
+          type: DataTypes.STRING(300), // 주소
           allowNull: false,
         },
         detailAddress: {
-          type: DataTypes.STRING(300),
+          type: DataTypes.STRING(300), // 디테일 주소
           allowNull: false,
         },
         remark: {
-          type: DataTypes.STRING(300),
+          type: DataTypes.STRING(300), // 특이사항
           allowNull: true,
         },
         isFilming: {
-          type: DataTypes.BOOLEAN,
+          type: DataTypes.BOOLEAN, // 상자보관 찰영유무
           allowNull: false,
           defaultValue: false,
         },
         imagePath: {
-          type: DataTypes.STRING(600),
+          type: DataTypes.STRING(600), // 사진
           allowNull: false,
         },
         isDelete: {
